2a5d9af97f11a0589b906ebfc81e5e5615344f92
ASUS <ASUS@LAPTOP-CG2DBI2M>
2019-07-12 17:43:01 +0530

    First commite

---
+++ JavaCore/src/Om.java
@@ -0,0 +1,12 @@
+
+public class Om {
+
+	public static void main(String[] args) {
+		char ch = '\u0950';
+		System.out.println(ch);
+		javax.swing.JOptionPane.showMessageDialog(null, ch);
+	}
+
+}
+
+
